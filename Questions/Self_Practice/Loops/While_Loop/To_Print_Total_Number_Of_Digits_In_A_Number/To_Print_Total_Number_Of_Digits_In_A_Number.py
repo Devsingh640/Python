13da75184b241e599b69c81cbbe1dbb810d8d01f
Devsingh640 <devsingh640@gmail.com>
2019-08-03 19:46:16 +0530

    PRACTICE QUESTIONS

--- Questions/Self_Practice/Loops/While_Loop/To_Print_Total_Number_Of_Digits_In_A_Number/To_Print_Total_Number_Of_Digits_In_A_Number.py
+++ Questions/Self_Practice/Loops/While_Loop/To_Print_Total_Number_Of_Digits_In_A_Number/To_Print_Total_Number_Of_Digits_In_A_Number.py
@@ -1,18 +1,18 @@
 #This is a program to print total no of digits in a number using a while loop.
 print('This is a program to print total no of digits in a number using a while loop.')
-value_n = int(input('Enter a value for number not starting with 0 : ')
+value_n = int(input('Enter a number is not starting with 0 : '))
 
-counter = 0 # setting counter to 0 that will hold no of digits count.
+counter = 0    # setting counter to 0 that will hold no of digits count.
 
-while True: # infinite loop. 
-    a = int(value_n / 10)   # calculating floor value.
-    value_n = a # calculated floor value is stored in value_n.
+while True:    # infinite loop. 
+    a = int(value_n / 10)    # calculating floor value.
+    value_n = a    # calculated floor value is stored in value_n.
     
-    if(a != 0): # this will be executed if a is not eaual to 0.
-        counter = counter + 1 # incrementing counter.
+    if(a != 0):    # this will be executed if a is not eaual to 0.
+        counter = counter + 1    # incrementing counter.
     else:
-        counter = counter + 1
-        break # this will break infinite loop.
+        counter = counter + 1    # incrementing counter.
+        break    # this will break infinite loop.
     
 print(" ")    
-print(counter) # printing count of digits.
+print(counter)    # printing count of digits.
